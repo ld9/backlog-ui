c1fa215c53b086538012b23c1a6b0461fef38088
Daniel Lynch <daniel@lynch.dev>
2021-04-23 13:57:49 -0400

    Implement Navigation Bar

---
+++ src/components/NavHeader.tsx
@@ -0,0 +1,44 @@
+import { NavLink, useLocation, useRouteMatch } from "react-router-dom";
+
+import '../styles/navbar.css';
+
+export default function NavHeader() {
+    let { url } = useRouteMatch();
+
+    let location = useLocation();
+    let path = location.pathname;
+    
+    let hideNavBar = [
+        "/",
+        "/login",
+        "/create-account"
+    ]
+
+    return (
+        <div>
+            { hideNavBar.includes(path) ? null : <NavHeaderContents/ > }
+        </div>
+    );
+}
+
+function NavHeaderContents() { 
+    return (
+        <div id='nav-container'>
+            <div id='nav-title-media'>
+                <div id="nav-user-home">
+                    <NavLink activeClassName='active-link' exact to="/user">Backlog</NavLink>
+                </div>
+                <div id='nav-media-links'>
+                    <NavLink activeClassName='active-link' to="/user/movies">Movies</NavLink>
+                    <NavLink activeClassName='active-link' to="/user/shows">Shows</NavLink>
+                    <NavLink activeClassName='active-link' to="/user/music">Music</NavLink>
+                </div>
+            </div>
+            <div id='nav-user-links'>
+                <NavLink activeClassName='active-link' to="/user/admin">Admin</NavLink>
+                <NavLink activeClassName='active-link' to="/user/preferences">Settings</NavLink>
+                <NavLink to="/login">Sign out User</NavLink>
+            </div>
+        </div>
+    );
+}
