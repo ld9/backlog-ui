58ecfaff443a98d739ac4929a921db178afbe013
Daniel Lynch <daniel@lynch.dev>
2021-04-23 13:59:04 -0400

    Create Font Size Selector

---
+++ src/components/preferences/FontSizeSelector.tsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+
+
+export default function ThemeInfo() {
+	
+	const [fontSize, setFontSize] = useState("1");
+
+	useEffect(() => {
+		// preserve theme name
+		if ((window as any).fontSize) {
+			setFontSize((window as any).fontSize);
+		} 
+
+		document.documentElement.style.setProperty('font-size', fontSize + 'em');
+
+	}, [fontSize]);
+
+	return (
+		<div className='preference-section'>
+			<div className='preference-heading'>
+				<h2>Adjust Font Size</h2>
+				<p>Change the size of text on the site</p>
+			</div>
+			
+			<div className='preference-contents'>
+				<input type='range' min='0.75' max='1.25' step='0.025' value={fontSize}
+					onChange={(event) => {
+                        (window as any).fontSize = event.target.value;
+                        setFontSize(event.target.value)
+                    }} />
+			</div>
+		</div>
+	);
+}
