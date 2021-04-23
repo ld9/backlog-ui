24eb95a6acb48c90876d77c51f0afe53e512dfd0
Daniel Lynch <daniel@lynch.dev>
2021-04-23 13:57:19 -0400

    Create a Signed-In User Homepage

---
+++ src/routes/user/Dashboard.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import AudioThumb from '../../components/AudioThumb';
+import AudioFile from '../../types/AudioFile';
+import VideoFile from '../../types/VideoFile';
+import VideoThumb from '../../components/VideoThumb';
+
+import "../../styles/userhome.css";
+
+export default function Dashboard() {
+    
+    let demoTrack: AudioFile = {
+        title: "Example Song Title",
+        artist: "John Smith",
+        album: "Greatest Hits",
+        art_uri: "https://i.pinimg.com/474x/05/23/a2/0523a2adeb72ac0835178cbba3d34b85.jpg"
+    }
+
+    let demoVideo: VideoFile = {
+        title: "Demo Movie or Documentary Title",
+        horiz_uri: "https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823__340.jpg",
+        meta: {
+            genre: "Void",
+            year: "Void",
+            tags: []
+        }
+    }
+
+    return (
+        <div className="homepage-section-parent">
+            <div className="homepage-section">
+                <h1>Dashboard</h1>
+            </div>
+            <div className="homepage-section">
+                <h2>
+                    You Recently Listened To... 
+                </h2>
+                <div>
+                    <div className="test-contain-tracks">
+                        {[...Array(12)].map((x, i) => 
+                            <AudioThumb key={i} track={demoTrack}></AudioThumb>
+                        )}
+                    </div>
+                </div>
+            </div>
+            <div className="homepage-section">
+                <h2>
+                    Continue Watching...
+                </h2>
+                <div className="test-contain-videos">
+                    {[...Array(8)].map((x, i) => 
+                        <VideoThumb key={i} video={demoVideo}></VideoThumb>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+}
