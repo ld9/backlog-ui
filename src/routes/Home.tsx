3c5ab860d1bebf5eb46da3b21d3ef6283d98465f
Daniel Lynch <daniel@lynch.dev>
2021-04-23 13:43:27 -0400

    Create Landing Page

---
+++ src/routes/Home.tsx
@@ -0,0 +1,26 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+
+import '../styles/home.css'
+
+export default function Home() {
+
+    
+
+    return (
+        <div className='grid-container'>
+            <div className='title-header'>
+                <h1>Demo Project Name</h1>
+                {/* <h2>{ auth.user ? "false" : "true" }</h2> */}
+            </div>
+            <div className='link-box'>
+                <span>
+                    <Link to='/create-account'>Create Account</Link>
+                </span>
+                <span>
+                    <Link to='/login'>Log In</Link>
+                </span>
+            </div>
+        </div>
+    );
+}
