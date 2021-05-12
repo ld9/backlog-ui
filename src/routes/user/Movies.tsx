85ea13163e3d5a91d1f15a95df43b2f838d9c13b
Daniel Lynch <daniel@lynch.dev>
2021-05-12 19:47:27 -0400

    Load in the movies from the API

--- src/routes/user/Movies.tsx
+++ src/routes/user/Movies.tsx
@@ -1,36 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { createGlobalState } from "react-hooks-global-state";
+import { initialState } from "../../App";
 import SortedPanel from "../../components/SortedVideoPanel";
 import VideoFile from "../../types/VideoFile";
+import { BASE_API_URL } from "../../variables";
 
 export default function Movies() {
-    
-    var groupingKeys: string[] = [
-        'Movies',
-        'Genres',
-        'Year',
-        'Tags'
-    ];
-    
-    var genres = ['Action', 'Sci-Fi', 'Adventure', 'Drama', 'Comedy'];
+  
+  const { useGlobalState } = createGlobalState(initialState);
+  const [token, setToken] = useGlobalState("token");
 
-    var demoMovies:VideoFile[] = [];
-    for (let i = 0; i < 20; i++) {
-        let demoFile:VideoFile = {
-            title: "Test Movie",
-            horiz_uri: "",
-            meta: {
-                genre: genres[Math.floor(Math.random()*genres.length)],
-                year: (1980+Math.floor(Math.random()*40)).toString(),
-                tags: ['funny', 'superhero']
-            }
-            
-        }
-        demoMovies.push(demoFile);
-    }
+  const [userMovies, setUserMovies] = useState([]);
 
-    return (
-        <div>
-            <SortedPanel groupingKeys={groupingKeys} sortableItems={demoMovies} ></SortedPanel>
-        </div>
-    );
+  useEffect(() => {
+    // Do it async, boys
+    (async () => {
+      const media = await fetch(`${BASE_API_URL}/api/media`, {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      });
+
+      const json = await media.json();
+      const videos = json.filter((item: { type: string; }) => {
+          let type: string = item.type;
+          return type.startsWith('video');
+      })
+
+      setUserMovies(videos);
+    })();
+    
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  var groupingKeys: string[] = ["Movies", "Genres", "Year", "Tags"];
+
+  return (
+    <div>
+      <SortedPanel
+        groupingKeys={groupingKeys}
+        sortableItems={userMovies}
+      ></SortedPanel>
+    </div>
+  );
 }
+
+
+// title: "Test Movie",
+// horiz_uri: "",
+// meta: {
+//     genre: genres[Math.floor(Math.random()*genres.length)],
+//     year: (1980+Math.floor(Math.random()*40)).toString(),
+//     tags: ['funny', 'superhero']
+// }
