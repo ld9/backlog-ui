62cd09b23c56416d86f870c2c9d8923a61159dbd
Daniel Lynch <daniel@lynch.dev>
2021-09-16 19:18:55 +0000

    Update Stage - obtain token for viewing + autoplay

--- src/routes/user/Stage.tsx
+++ src/routes/user/Stage.tsx
@@ -13,10 +13,12 @@ import {
   MessageInform,
   SocketBeaconMessageType,
 } from "../../types/SocketInfoMessage";
+import { BASE_API_URL, BASE_CONTENT_URL } from "../../variables";
 
 export default function Stage() {
   const [stage, setStage] = useGlobalState("stage");
   const [user, setUser] = useGlobalState("user");
+  const [token, setToken] = useGlobalState("token");
 
   const location = useLocation();
   const history = useHistory();
@@ -24,10 +26,12 @@ export default function Stage() {
 
   const [isActive, setIsActive] = useState(false);
   const [isFull, setIsFull] = useState(false);
+  const [completeCount, setCompleteCount] = useState(0);
 
   // Makes sure that re-renders happen when needed
   const [msgCount, setMsgCount] = useState(0);
 
+  const [myWatchKeys, setMyWatchKeys] = useState({});
   const [watchers, setWatchers] = useState({});
 
   const currentBuffer = () => {
@@ -77,6 +81,7 @@ export default function Stage() {
   }
 
   // When the Video element changes, set the video's event listeners
+  // When the video is finished, play the next one.
   useEffect(() => {
     function sendInformMessage() {
       if (video.current != null) {
@@ -100,8 +105,48 @@ export default function Stage() {
           sendInformMessage();
         }
       };
+
+      // Effectively re-runs this useEffect.
+      video.current.onended = function () {
+        setCompleteCount(completeCount + 1);
+      }
     }
-  }, [video, user]);
+
+    // Maybe this should just be in the onended function?
+    if (
+      video.current != null &&
+      video.current.currentTime == video.current.duration
+    ) {
+      console.log("next");
+
+      let queueItem;
+      for (let i = 0; i < stage.queue.length; i++) {
+        if (
+          video.current.src
+            .substr(video.current.src.lastIndexOf("/") + 1)
+            .split("?")[0] == stage.queue[i].uri
+        ) {
+          queueItem = stage.queue[i + 1];
+          break;
+        }
+      }
+      if (queueItem == undefined) {
+        return;
+      }
+
+      let storedKeyJson = localStorage.getItem(`mediaKey-${queueItem._id}`);
+
+      if (storedKeyJson != null) {
+        let storedKey = JSON.parse(storedKeyJson);
+
+        console.log(stage.queue, storedKey);
+        video.current.src = `${BASE_CONTENT_URL}/content/restrict/${queueItem.uri}?token=${storedKey.token}`;
+        console.log("updating source");
+      } else {
+        console.error("could not find key for media item");
+      }
+    }
+  }, [video, user, completeCount]);
 
   // Sets the activity of the videoplayer (large/small)
   useEffect(() => {
@@ -201,6 +246,66 @@ export default function Stage() {
     });
   }, [stage, stage.queue]);
 
+  useEffect(() => {
+    async function setInitialVideo(sources: any) {
+      // Sets the current video if there isn't a source set yet.
+      if (
+        video.current !== null &&
+        video.current.src == "" &&
+        stage.queue.length > 0
+      ) {
+        console.log(
+          stage.queue[0],
+          await sources,
+          await sources[stage.queue[0]._id]
+        );
+        video.current.src = `${BASE_CONTENT_URL}/content/restrict/${
+          stage.queue[0].uri
+        }?token=${sources[stage.queue[0]._id].token}`;
+        console.log("set source");
+      }
+    }
+
+    (async () => {
+      let temp: { [key: string]: any } = {};
+
+      // Load the tokens to let the user authenticate when requesting video files
+      stage.queue.forEach(async (queueItem) => {
+        let storedKey = localStorage.getItem(`mediaKey-${queueItem._id}`);
+        let mediaToken;
+        console.log(storedKey);
+        if (storedKey === null) {
+          let newKey = fetch(`${BASE_API_URL}/user/request-media-token`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              token,
+              mediaId: queueItem.uri,
+            }),
+          }).then((body) => body.json());
+
+          mediaToken = await newKey;
+          localStorage.setItem(
+            `mediaKey-${queueItem._id}`,
+            JSON.stringify(await newKey)
+          );
+
+          temp[queueItem._id] = mediaToken;
+          setInitialVideo(temp);
+        } else {
+          mediaToken = JSON.parse(await storedKey);
+          temp[queueItem._id] = mediaToken;
+          setInitialVideo(temp);
+        }
+      });
+
+      // Saves the keys (TODO: Do even need to do this?)
+      setMyWatchKeys(temp);
+    })();
+  }, [stage.queue]);
+
   const formattedSeconds = (seconds: number = 0) => {
     const hhmmss: string = new Date(Math.floor(seconds) * 1000)
       .toISOString()
@@ -222,32 +327,22 @@ export default function Stage() {
       <div id="stage-pseudo-header">A</div>
       <div id="stage-content">
         <div id="stage-player">
-          <video id="stage-player-video" ref={video} controls muted>
-            <source
-              src="https://lynch.dev/vidtest/train_1.mp4"
-              type="video/mp4"
-            />
+          <video id="stage-player-video" ref={video} controls muted autoPlay>
+            <source type="video/mp4" />
           </video>
         </div>
         <div id="stage-extra-content">
           <div id="global-controls">
             <h3>Global Controls</h3>
-            <button
-              onClick={() => {
-                if (
-                  stage.name !== "local" &&
-                  !window.confirm("Really abandon this room?")
-                ) {
-                  return;
-                }
-
-                history.push("/user/stage/create");
-              }}
-            >
-              {stage.name === "local"
-                ? "Create joinable room"
-                : "Abandon & create new room"}
-            </button>
+            {stage.name === "local" ? (
+              <button
+                onClick={() => {
+                  history.push("/user/stage/create");
+                }}
+              >
+                Create Joinable Room
+              </button>
+            ) : null}
             <button onClick={requestPause}>Pause All</button>
             <button onClick={requestResume}>Play All</button>
             <button onClick={requestSeek}>Jump to Me</button>
@@ -256,7 +351,7 @@ export default function Stage() {
             <div id="stage-watchers">
               <h3>Stage Audience</h3>
               {Object.values(watchers).map(
-                (watcher: any /* FIX ME!! */, key: number) => (
+                (watcher: any /* FIX ME!! UNTYPED */, key: number) => (
                   <div key={key} className="stage-watcher">
                     <div className="stage-watcher-name">
                       {watcher.status.name}
--- src/types/MediaItem.ts
+++ src/types/MediaItem.ts
@@ -1,12 +1,15 @@
 export default interface MediaItem {
+    _id: any;
     uri: string;
     meta: {
         title: string;
         released?: number;
         thumb?: string;
+        tmdb?: any;
         /* Allow any meta things to be present. */
         [key: string]: any;
     };
     tags: [];
     type: string;
+    sortTag: any;
 }
