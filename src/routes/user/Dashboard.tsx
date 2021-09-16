c3fc30b3720836ef83fd3d017fe56c2469ef60fa
Daniel Lynch <daniel@lynch.dev>
2021-09-16 19:17:34 +0000

    Reformat Code (auto)

--- src/routes/user/Dashboard.tsx
+++ src/routes/user/Dashboard.tsx
@@ -1,65 +1,78 @@
-import React from 'react';
-import AudioThumb from '../../components/AudioThumb';
-import AudioFile from '../../types/AudioFile';
-import VideoFile from '../../types/VideoFile';
-import VideoThumb from '../../components/VideoThumb';
+import React from "react";
+import AudioThumb from "../../components/AudioThumb";
+import AudioFile from "../../types/AudioFile";
+import VideoFile from "../../types/VideoFile";
+import VideoThumb from "../../components/VideoThumb";
 
 import "../../styles/userhome.css";
-import { strings } from '../../strings';
-import SocketTest from '../../components/SocketTest';
-import { useHistory } from 'react-router-dom';
+import { strings } from "../../strings";
+import SocketTest from "../../components/SocketTest";
+import { useHistory } from "react-router-dom";
+import { useGlobalState } from "../../state";
+import { ToastType } from "../../types/ToastType";
 
 export default function Dashboard() {
+  const [toasts, setToasts] = useGlobalState("toasts");
 
-    const history = useHistory();
-    
-    let demoTrack: AudioFile = {
-        title: "Example Song Title",
-        artist: "John Smith",
-        album: "Greatest Hits",
-        art_uri: "https://i.pinimg.com/474x/05/23/a2/0523a2adeb72ac0835178cbba3d34b85.jpg"
-    }
+  const history = useHistory();
 
-    // let demoVideo: VideoFile = {
-    //     title: "Demo Movie or Documentary Title",
-    //     horiz_uri: "https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823__340.jpg",
-    //     meta: {
-    //         genre: "Void",
-    //         year: "Void",
-    //         tags: []
-    //     }
-    // }
+  let demoTrack: AudioFile = {
+    title: "Example Song Title",
+    artist: "John Smith",
+    album: "Greatest Hits",
+    art_uri:
+      "https://i.pinimg.com/474x/05/23/a2/0523a2adeb72ac0835178cbba3d34b85.jpg",
+  };
 
-    return (
-        <div className="homepage-section-parent">
-            <div className="homepage-section">
-                <h1>{strings.dash_title}</h1>
-            </div>
-            <div className="homepage-section">
-                <h2>
-                    {strings.dash_listened}
-                </h2>
-                <div>
-                    <div className="test-contain-tracks">
-                        {[...Array(12)].map((x, i) => 
-                            <AudioThumb key={i} track={demoTrack}></AudioThumb>
-                        )}
-                    </div>
-                </div>
-            </div>
-            <div className="homepage-section">
-                <h2>
-                    {strings.dash_watched}
-                </h2>
-                {/* <div className="test-contain-videos">
+  // let demoVideo: VideoFile = {
+  //     title: "Demo Movie or Documentary Title",
+  //     horiz_uri: "https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823__340.jpg",
+  //     meta: {
+  //         genre: "Void",
+  //         year: "Void",
+  //         tags: []
+  //     }
+  // }
+
+  return (
+    <div className="homepage-section-parent">
+      <div className="homepage-section">
+        <h1>{strings.dash_title}</h1>
+      </div>
+      <div className="homepage-section">
+        <h2>{strings.dash_listened}</h2>
+        <div>
+          <div className="test-contain-tracks">
+            {[...Array(12)].map((x, i) => (
+              <AudioThumb key={i} track={demoTrack}></AudioThumb>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="homepage-section">
+        <h2>{strings.dash_watched}</h2>
+        {/* <div className="test-contain-videos">
                     {[...Array(8)].map((x, i) => 
                         <VideoThumb key={i} video={demoVideo}></VideoThumb>
                     )}
                 </div> */}
 
-                <SocketTest></SocketTest>
-                <button onClick={() => {history.push('/user/stage/test')}}>Join stage "test"</button>
-            </div>
-        </div>
-    );
+        <SocketTest></SocketTest>
+        <button
+          onClick={() => {
+            history.push("/user/stage/test");
+          }}
+        >
+          Join stage "test"
+        </button>
+        <button
+          onClick={() => {
+            setToasts([...toasts, { content: "asdasd", type: ToastType.GOOD, until: (Date.now() + 3000) }]);
+          }}
+        >
+          Test toasts
+        </button>
+      </div>
+    </div>
+  );
 }
